b8471c199da888b1a893a4c0c2c8d97ba6ee495a
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-07-02 20:41:03 -0300

    Lista sequencional e dicionario json

---
+++ Tipos sequenciais e dicionários/Dicionario.py
@@ -0,0 +1,4 @@
+pessoas={ 1111:["nome01"], 2222:["nome2"], 3333:["nome3"]}
+
+print(f"primeria pessoa= {pessoas[1111]}")
+
---
+++ Tipos sequenciais e dicionários/Lista-Sequencional.py
@@ -0,0 +1,9 @@
+lista = [101, 200, 150, 122, 136, 505]
+
+print (f"lista[0] = {lista[0]}")   #Retorno =  lista[0] = 101
+print (f"lista[1] = {lista[1]}")   #Retorno =  lista[1] = 200
+print (f"lista[2] = {lista[2]}")   #Retorno =  lista[2] = 150
+print (f"lista[3] = {lista[3]}")   #Retorno =  lista[3] = 122
+print (f"lista[4] = {lista[4]}")   #Retorno =  lista[4] = 136
+print (f"lista[5] = {lista[5]}")   #Retorno =  lista[5] = 505
+print (f"lista completa = {lista}")   #Retorno =  lista[ completa = [101, 200, 150, 122, 136, 505]
