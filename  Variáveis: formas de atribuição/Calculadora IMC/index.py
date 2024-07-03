957b94f4e23517e778728b910aeb40493bb5cc08
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-07-03 20:17:20 -0300

    Conclusão Python basico

---
+++  Variáveis: formas de atribuição/Calculadora IMC/index.py
@@ -0,0 +1,16 @@
+nome = input("Qual seu nome?")
+print("Bem vindo " +nome)
+
+
+altura = eval(input(nome+ " qual sua altura?"))
+peso = eval(input(nome+ " qual seu peso?"))
+
+
+resultado = peso / (altura * altura)
+
+print("O seu IMC é: {} " .format(resultado))
+
+
+y= 2+3*5.
+ 
+print(y)
---
+++  Variáveis: formas de atribuição/Exercicios1.py
@@ -0,0 +1,14 @@
+a = ["10"]
+b = ["20"]
+c = ["30"]
+
+r = a+b+c
+r1 = a*2+b*3+c*4
+
+print(r)
+print(r1)
+
+a1 = 10
+b1 = 5
+x = -b1/a1
+print("x= {}" .format(x))
