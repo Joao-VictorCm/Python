aa8eb54a467d0eccbc08ce50c1d13086841b7247
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-07-15 21:43:12 -0300

    Calculando fatorial_recurursivo de um numero

--- Emprego de procedimentos e funções/Exercicios.py
+++ Emprego de procedimentos e funções/Exercicios.py
@@ -5,18 +5,18 @@
 
 
 def ehPar(n):
-    r =(n%2==0)
+    r =(n%2==0)  #Primeiro verificar quais numeros são pares
     return r
 
 def soma_pares(lista):
     soma = 0
     for num in lista:
         if(ehPar(num)):
-            soma = soma + num
+            soma = soma+num
             return soma
         
 
-lista_teste = [1, 2, 3, 5, 8]
+lista_teste = [1, 2, 3, 5, 4]
 soma = soma_pares(lista_teste)
 print("Soma é: [{}]".format(soma))
 
@@ -24,3 +24,13 @@ print("Soma é: [{}]".format(soma))
 # Exercicio 2
 
 # Implementar uma solução em Python que calcule o fatorial de um número
+
+def fatorial_recursivo(n):
+    if((n==0) or (n==1)):
+        return 1
+    return n*fatorial_recursivo(n-1)
+   
+    
+numero = 4
+
+print(f"o Fatorial de {numero} é: {fatorial_recursivo(numero)} ")
