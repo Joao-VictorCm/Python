9992789b51f94c4ded9077ef427958e239f502fe
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-07-28 19:29:23 -0300

    Gerando graficos com Python

--- Aplicando as bibliotecas Python para a resolução de problemas reais/Exemplos.py
+++ Aplicando as bibliotecas Python para a resolução de problemas reais/Exemplos.py
@@ -1,3 +1,4 @@
+# Exercico 01
 # Impletmentar uma solução em Python para visualizar dados de 
 # vendas de produtos em um grafico de barras
 
@@ -12,4 +13,18 @@ plt.bar(x, y)
 plt.xlabel("x letras")
 plt.ylabel("y numeros")
 plt.title("grafico ex 01")
+plt.show()
+
+
+# Exercico 02
+# Implentar uma solução para gerar 1000 pontos seguindo a Distribuição Normal
+# com media 20 e desvio padrão 
+# Alem disso, exiba os dados em um hispograma
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+np.random.seed(1)
+dados = np.random.normal(loc=20, scale=2, size=1000)
+plt.hist(dados, color= "lightblue", ec ="red")
 plt.show()
