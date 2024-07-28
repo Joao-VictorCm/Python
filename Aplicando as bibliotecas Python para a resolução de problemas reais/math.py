911f5a704977ff5980a7e61874ad687922fba152
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-07-28 20:10:09 -0300

    Modulos mais usados no mercado de trablaho em Python

---
+++ Aplicando as bibliotecas Python para a resolução de problemas reais/math.py
@@ -0,0 +1,9 @@
+# Função 	Retorno
+# sqrt(x) 	Raiz quadrada de x
+# ceil(x) 	Menor inteiro maior ou igual a x
+# floor(x) 	Maior inteiro menor ou igual a x
+# cos(x) 	Cosseno de x
+# sin(x) 	Seno de x
+# log(x, b) 	Logaritmo de x na base b
+# pi 	Valor de Pi (3.141592...)
+# e 	Valor de e (2.718281...)
---
+++ Aplicando as bibliotecas Python para a resolução de problemas reais/random.py
@@ -0,0 +1,26 @@
+# Função 	Retorno
+# random() 	Número de ponto flutuante no intervalo (00,0, 1,0)
+# uniform(a, b) 	Número de ponto flutuante N tal que a ≤ N ≤ b
+# gauss(mu, sigma) 	Distribuição gaussiana. mu é a média e sigma é o desvio padrão.
+# normalvariate(mu, sigma) 	Distribuição gaussiana. mu é a média e sigma é o desvio padrão.
+
+
+
+# Funções para números inteiros
+
+# Veja algumas das principais funções disponíveis para inteiros no módulo random.
+# Função 	Retorno
+# randrange(stop) 	Um elemento selecionado aleatório de range(start, stop, step), mas sem construir um objeto range.
+# randrange(start, stop, [step]) 	
+# randint(a, b) 	Número inteiro N tal que a ≤ N ≤ b
+
+
+
+
+# Funções para sequências
+
+# Esta tabela mostra algumas das principais funções disponíveis para sequências no módulo random:
+# Função 	Retorno
+# choice(seq) 	Elemento aleatório de uma sequência não vazia seq.
+# shuffle(x[, random]) 	Embaralha a sequência x no lugar.
+# sample(pop, k) 	Uma sequência de tamanho k de elementos escolhidos da população pop, sem repetição. Usada para amostragem sem substituição.
